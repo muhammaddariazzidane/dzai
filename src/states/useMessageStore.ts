8a18a4d531c5af47c5bf555453dfdc1a9f64f32f
muhammaddariazzidane <dariazzidane@gmail.com>
2025-08-31 20:41:07 +0700

    update order fetchConversations

--- src/states/useMessageStore.ts
+++ src/states/useMessageStore.ts
@@ -77,7 +77,7 @@ export const useMessageStore = create<MessageStore>((set) => ({
       query(
         collection(db, "conversations"),
         where("userId", "==", userId),
-        orderBy("startedAt", "asc")
+        orderBy("startedAt", "desc")
       ),
       (snapshot) => {
         const fetchedConversations: ConversationType[] = snapshot.docs.map(
